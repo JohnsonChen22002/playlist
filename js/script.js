c78ba56a53c674de6f861636b2196f7ad88afce5
Johnson Chen <johnsonchen12345@gmail.com>
2018-02-28 23:34:26 +0000

    PlaylistSong

--- js/script.js
+++ js/script.js
@@ -23,95 +23,55 @@ var mySong = {
 	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
 }
 
+var myPlayList = [
+	{
+		"title":"24K Magic",
+		"artist":"Bruno Mars",
+		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+	},
+	{
+		"title":"Sir Duke",
+		"artist":"Stevie Wonder",
+		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
+		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+	},
+	{
+		"title":"Sorry",
+		"artist":"Justin Bieber",
+		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
+		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+	}
+
+]
+
 
 
 // DOCUMENT READY FUNCTION
 $( document ).ready(function() {
-//   $('button').click(function(){
-  	
-// 	var titleInput = $("#title").val();
-//     var artistInput = $("#artist").val();
-//     var linkInput = $("#play-link").val();
-//     var albumimageInput = $("#album-image").val();
-    
-// var myPlayList = [
-// 	{
-// 		"title":"24K Magic",
-// 		"artist":"Bruno Mars",
-// 		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-// 		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
-// 	},
-// 	{
-// 		"title":"Sir Duke",
-// 		"artist":"Stevie Wonder",
-// 		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-// 		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
-// 	},
-// 	{
-// 		"title":"Sorry",
-// 		"artist":"Justin Bieber",
-// 		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-// 		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
-// 	}
-
-// ]
-// groceryList.push({itemName: itemInput, quantity: quantityInput});
-//     displayList();
-// });
+clearList();
+displayList();
 
 
-
-// });
-
-// function displayList(){
-// 	for (var i = 0; i < groceryList.length; i++){
-//         $('#list').append('<p>' + groceryList[i].itemName +'<p>');
-//         $('#list').append('<p>' + groceryList[i].quantity +'<p>');
-//     }
-// }
-
-// function clearList(){
-// 	$('#list').empty();
-  
-  
-// }
-
-// function addSong(){
- 
-  
-  
-// }
-// displayList();
-
-var groceryList = [
-];
-
-// Update the click function
-$('button').click(function(){
-var itemInput = $("#item").val();
-var quantityInput = $("#quantity").val();
-groceryList.push({itemName: itemInput, quantity: quantityInput});
-    displayList();
 });
 
-
 function displayList(){
-    for (var i = 0; i < groceryList.length; i++){
-        $('#list').append('<p>' + groceryList[i].itemName +'<p>');
-        $('#list').append('<p>' + groceryList[i].quantity +'<p>');
+for (var i = 0; i < myPlayList.length; i++){
+    $('#songs').append('<p>Title: ' + myPlayList[i].title +'<p>');
+    $('#songs').append('Artist: ' + myPlayList[i].artist +'<p>');
+    $('#songs').append(' Mp3: ' + myPlayList[i]["mp3-url"] +'<p>');
+    $('#songs').append(' Image: ' + myPlayList[i]["image-url"] +'<p>');
     }
+
+  
 }
 
 function clearList(){
-    $('#list').empty();
+   $('#songs').empty();
 }
 
-//Update the add item function below
-function addItem(){
+function addSong(){
  
- 
- 
+  
+  
 }
-
-displayList();
-});
