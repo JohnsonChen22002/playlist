a68934da1d9550dd2d603dca64439c6b3197ea35
Johnson Chen <johnsonchen12345@gmail.com>
2018-03-03 14:59:44 +0000

    Playlist Draft

--- js/script.js
+++ js/script.js
@@ -1,77 +1,63 @@
 // quick fix for Cloud9 warning:
 /* global $ */
-
-// Class 2:
-// Complete displayList() to show a single song in the list
-
-// Class 2:
-// Complete displayList() to show all the songs in the list
-// Complete clearList() funcion
-
-// Class 3:
-// Complete the addSong function to take an input from input boxes and push a new song to the playlist array. 
-
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-
-// Songs
 var mySong = {
-	"title":"24K Magic",
-	"artist":"Bruno Mars",
-	"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+	"title": "24K Magic",
+	"artist": "Bruno Mars",
+	"mp3-url": "https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+	"image-url": "https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
 }
 
-var myPlayList = [
-	{
-		"title":"24K Magic",
-		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+var myPlayList = [{
+		"title": "24K Magic",
+		"artist": "Bruno Mars",
+		"mp3-url": "https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+		"image-url": "https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
 	},
 	{
-		"title":"Sir Duke",
-		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"title": "Sir Duke",
+		"artist": "Stevie Wonder",
+		"mp3-url": "https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
+		"image-url": "https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
 	},
 	{
-		"title":"Sorry",
-		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"title": "Sorry",
+		"artist": "Justin Bieber",
+		"mp3-url": "https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
+		"image-url": "http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
 	}
 
-]
+];
 
 
 
 // DOCUMENT READY FUNCTION
-$( document ).ready(function() {
-clearList();
-displayList();
+$(document).ready(function() {
+	$('button').click(function() {
+		clearList(); 
+		addSong();
+		displayList();
+	});
 
 
 });
 
-function displayList(){
-for (var i = 0; i < myPlayList.length; i++){
-    $('#songs').append('<p>Title: ' + myPlayList[i].title +'<p>');
-    $('#songs').append('Artist: ' + myPlayList[i].artist +'<p>');
-    $('#songs').append(' Mp3: ' + myPlayList[i]["mp3-url"] +'<p>');
-    $('#songs').append(' Image: ' + myPlayList[i]["image-url"] +'<p>');
-    }
+function displayList() {
+	for (var i = 0; i < myPlayList.length; i++) {
+		$('#songs').append(' Title: ' + myPlayList[i].title + '<p>');
+		$('#songs').append(' Artist: ' + myPlayList[i].artist + '<p>');
+		$('#songs').append(' Mp3: ' + myPlayList[i]["mp3-url"] + '<p>');
+		$('#songs').append(' Image: ' + myPlayList[i]["image-url"] + '<p>');
+	}
 
-  
+
 }
 
-function clearList(){
-   $('#songs').empty();
+function clearList() {
+	$('#songs').empty();
 }
 
-function addSong(){
- 
-  
-  
+function addSong() {
+	myPlayList.push({ title: $("#title").val(), artist: $("#artist").val(), ["mp3-url"]: $("#mp3").val(), ["image-url"]: $("#image").val() });
+
+
 }
